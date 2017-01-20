7949de68c6a9f6d4895b0281510adf76b1e848a8
Jeremy <jeremym@teamtreehouse.com>
2017-01-20 14:54:40 -0800

    Changed Point.cs to reflect our starting code.

--- TreehouseDefense/Point.cs
+++ TreehouseDefense/Point.cs
@@ -13,17 +13,6 @@ namespace TreehouseDefense
             Y = y;
         }
 
-        public override bool Equals(object obj)
-        {
-            Point that = obj as Point;
-            return that != null && this.X == that.X && this.Y == that.Y;
-        }
-
-        public override int GetHashCode()
-        {
-            return X.GetHashCode() * 31 + Y.GetHashCode();
-        }
-
         public double DistanceTo(Point point)
         {
             return Math.Sqrt(Math.Pow(X - point.X, 2.0) + Math.Pow(Y - point.Y, 2.0));
