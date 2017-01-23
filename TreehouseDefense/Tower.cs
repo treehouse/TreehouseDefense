aedf414ae544314da3aa29465b7b3ff6d1b78f60
Jeremy McLain <jeremym@teamtreehouse.com>
2017-01-23 10:14:04 -0800

    Added Tower.cs

---
+++ TreehouseDefense/Tower.cs
@@ -0,0 +1,27 @@
+﻿namespace TreehouseDefense
+{
+    public class Tower
+    {
+        protected virtual int Range { get; } = 1;
+        protected virtual int Power { get; } = 1;
+
+        private readonly MapLocation _location;
+
+        public Tower(MapLocation location)
+        {
+            _location = location;
+        }
+
+        public void FireOnInvaders(IInvader[] invaders)
+        {
+            foreach (IInvader invader in invaders)
+            {
+                if (invader.IsActive && _location.InRangeOf(invader.Location, Range))
+                {
+                    invader.DecreaseHealth(Power);
+                    break;
+                }
+            }
+        }
+    }
+}
