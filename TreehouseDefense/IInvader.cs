56455464410865d298841b0634d39be350cebc7d
Jeremy McLain <jeremym@teamtreehouse.com>
2017-01-23 10:04:59 -0800

    Added IInvader.cs

---
+++ TreehouseDefense/IInvader.cs
@@ -0,0 +1,14 @@
+﻿namespace TreehouseDefense
+{
+    public interface IInvader
+    {
+        MapLocation Location { get; }
+        bool HasScored { get; }
+        int Health { get; }
+        bool IsNeutralized { get; }
+        bool IsActive { get; }
+
+        void DecreaseHealth(int factor);
+        void Move();
+    }
+}
