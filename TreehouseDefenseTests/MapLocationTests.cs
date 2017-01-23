be7c99848dd8e030fafb476fc8b2e7d15595b466
Jeremy McLain <jeremym@teamtreehouse.com>
2017-01-22 22:02:24 -0800

    Added MapTests.cs

--- TreehouseDefenseTests/MapLocationTests.cs
+++ TreehouseDefenseTests/MapLocationTests.cs
@@ -1,10 +1,4 @@
 ﻿using Xunit;
-using TreehouseDefense;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TreehouseDefense.Tests
 {
---
+++ TreehouseDefenseTests/MapTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace TreehouseDefense.Tests
+{
+    public class MapTests
+    {
+        private Map _map3 = new Map(3, 3);
+
+        [Fact()]
+        public void MapSizeTooSmall()
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => new Map(0, 0));
+        }
+
+        [Fact()]
+        public void PointIsOnMap()
+        {
+            var target = _map3;
+            Assert.True(target.OnMap(new Point(1, 2)));
+        }
+
+        [Fact()]
+        public void PointIsNotOnMap()
+        {
+            var target = _map3;
+            Assert.False(target.OnMap(new Point(3, 3)));
+        }
+    }
+}
